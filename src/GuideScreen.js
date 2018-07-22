dcf71f73c5cfc2ce99d7ac147c31e7b005f9494e
Fiona Rowan <fmr2112@columbia.edu>
2018-07-22 17:01:50 -0400

    add google maps package and add navigation buttons to header

--- src/GuideScreen.js
+++ src/GuideScreen.js
@@ -3,7 +3,21 @@ import { Text, View, Button } from 'react-native';
 import styles from './styles';
 
 class GuideScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Guide',
+      headerRight: (
+        <Button
+          onPress={() => navigation.navigate('Map')}
+          title="Go to Map"
+          color="#bbb"
+        />
+      ),
+    };
+  };
+
   render() {
+
     return (
       <View style={styles.container}>
         <Text>Guide</Text>
--- src/MapScreen.js
+++ src/MapScreen.js
@@ -3,6 +3,19 @@ import { Text, View, Button } from 'react-native';
 import styles from './styles';
 
 class MapScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Map',
+      headerRight: (
+        <Button
+          onPress={() => navigation.navigate('Guide')}
+          title="Go to Guide"
+          color="#bbb"
+        />
+      ),
+    };
+  };
+
   render() {
     return (
       <View style={styles.container}>
